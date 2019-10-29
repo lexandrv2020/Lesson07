95456e15e19d9fc486463f193c1ee2294985369e
lexandrv <lexandrv@gmail.com>
2019-10-29 18:31:46 +0300

    Домашнее задание 07 - plus

--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@
 let day_string = document.querySelector('.day-string'),
     day = new Date();
 
-day_string.innerHTML = "Текущая дата/время: '" + formatNumber(day.getHours()) + ":" + formatNumber(day.getMinutes()) + ":" + formatNumber(day.getSeconds()) + " " + formatNumber(day.getDate()) + "." + formatNumber(day.getMonth()) + "." + formatNumber(day.getFullYear()) + "'";
+day_string.innerHTML = "Текущая дата/время: '<b>" + formatNumber(day.getHours()) + ":" + formatNumber(day.getMinutes()) + ":" + formatNumber(day.getSeconds()) + " " + formatNumber(day.getDate()) + "." + formatNumber(day.getMonth()) + "." + formatNumber(day.getFullYear()) + "</b>'";
 
 function formatNumber(value) {
     if (value < 10) {
